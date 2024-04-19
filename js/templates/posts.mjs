f4b140d577ef17094cdb1729cbe8c13aa599b6b3
h-bcode <jon.are.br@gmail.com>
2024-04-19 10:23:45 +0200

    Refactor renderAllPostsTemplates function to add click event on every card in posts.mjs

--- js/templates/posts.mjs
+++ js/templates/posts.mjs
@@ -1,6 +1,24 @@
 import { postTemplate } from "./postTemplate.mjs";
 
 export function renderAllPostsTemplates(postDataList, parent) {
-    const elements = postDataList.map(postTemplate).filter(el => el !== null);
-    parent.append(...elements);
+    try {
+        const elements = postDataList.map(postData  => {
+            const element = postTemplate(postData);
+            if (element) {
+                //Add click event on every card
+                element.style.cursor = "pointer"; 
+                element.addEventListener('click', () => {
+                    window.location.href = `/post/?id=${postData.id}`;
+                });
+            }
+            return element;
+        }
+        
+        ).filter(el => el !== null);
+        
+        parent.append(...elements);
+    } catch (error) {
+    console.error("Failed to render posts:", error);
+    }
+  
 }
