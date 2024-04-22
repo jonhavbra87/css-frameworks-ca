d236f4ef7d95bb4954d62fbb0e40e464cafbb04c
h-bcode <jon.are.br@gmail.com>
2024-04-22 21:56:49 +0200

    Refactor login form listener to handle user login and redirect to profile page

--- js/handlers/login.mjs
+++ js/handlers/login.mjs
@@ -1,4 +1,5 @@
 import { login } from "../api/auth/login.mjs"
+import * as storage from "../storage/index.mjs"
 
 export function setLoginFormListener() {
     const form = document.querySelector("#loginForm");
@@ -13,13 +14,19 @@ export function setLoginFormListener() {
             const profile = Object.fromEntries(formData.entries())
             console.log("I'm going to login the user with the following data: ", profile)
 
-            try {
-                await login(profile);
-                window.location.href = `/profile/?name=${profile.name}`;
+         try {
+            await login(profile);
+            const user = storage.load("profile");
+            if (user && user.name) {
+                window.location.href = `/profile/?name=${user.name}`;
+            } else {
+                throw new Error("Brukernavn er ikke lagret i localStorage.");
             }
-            catch (error) {
-                console.error("Login failed:", error);
-            }
+         } catch (error) {
+            console.error("Login failed:", error);
+         }
+           
+
         })
     }
 }
