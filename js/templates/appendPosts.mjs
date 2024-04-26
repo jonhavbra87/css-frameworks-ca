5e81fbef4c93104a000cfe4b5bd555eeb72d5b52
h-bcode <jon.are.br@gmail.com>
2024-04-26 20:20:13 +0200

    Add appendPosts function to append post elements to parent container

---
+++ js/templates/appendPosts.mjs
@@ -0,0 +1,19 @@
+import { postTemplate } from "./postTemplate.mjs";
+
+export function appendPosts(postDataList, parent) {
+    try {
+        postDataList.forEach(postData => {
+            const element = postTemplate(postData);
+            if (element) {
+                // Add click event on every card
+                element.style.cursor = "pointer"; 
+                element.addEventListener('click', () => {
+                window.location.href = `/post/?id=${postData.id}`;
+                });
+                parent.append(element);
+            }
+        });
+    } catch (error) {
+    console.error("Failed to render posts:", error);
+    }
+}
