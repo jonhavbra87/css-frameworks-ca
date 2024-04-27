d87b465476e70ca6013ad84232defd301eb9ba6f
h-bcode <jon.are.br@gmail.com>
2024-04-27 08:47:46 +0200

    Update getPosts function to include comment and reactions parameters

--- js/api/posts/read.mjs
+++ js/api/posts/read.mjs
@@ -3,14 +3,17 @@ import { API_SOCIAL_URL } from "../constants.mjs";
 import { doFetch } from "../helpers/doFetch.mjs";
 
 const action = "/posts"
-const author = "/?_author=true"
+const author = "?_author=true"
+const comment = "&_comments=true"
+const reactions = "&_reactions=true"
 const tag = "?_tag=my_tag"
 
 
 export async function getPosts() {
     // const updatePostURL = `${API_SOCIAL_URL}${action}${author}`;
     try {
-        const posts = await doFetch(API_SOCIAL_URL + action + author);
+        const posts = await doFetch(API_SOCIAL_URL + action + author + comment + reactions);
+        console.log(API_SOCIAL_URL + action + author + comment + reactions);
         return posts;
     } catch (error) {
         throw new Error(error)
