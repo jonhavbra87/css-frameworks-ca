90a72de87e17ba8fe2bc52ee72b78b206c0a6183
Jon Are <jon.are.br@gmail.com>
2024-05-10 18:13:35 +0200

    Add console.log statement to backButton function in backButton.mjs

--- js/listeners/backButton.mjs
+++ js/listeners/backButton.mjs
@@ -2,5 +2,6 @@ export function backButton() {
     const backButton = document.querySelector("#backButton");
     backButton.addEventListener("click", () => {
         window.history.back();
+        console.log("Back button clicked");
     });
 }
