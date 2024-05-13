6b9438429fa1d469112aeb438c44337c6d2a49eb
Jon Are <jon.are.br@gmail.com>
2024-05-13 09:17:08 +0200

    chore: Refactor doFetch function for improved error handling and write JSDOCS

--- js/api/helpers/doFetch.mjs
+++ js/api/helpers/doFetch.mjs
@@ -1,5 +1,20 @@
 import { authFetch } from "../authFetch.mjs";
 
+/**
+ * Fetches data from the provided URL with authentication.
+ *
+ * This function uses the function "authFetch" to make an authenticated request to the given URL,
+ * parses the response as JSON, and returns the resulting data. If an error occurs
+ * during the fetch or JSON parsing, an Error is thrown and an alert is displayed
+ * with the error message.
+ *
+ * @async
+ * @function doFetch
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} The JSON-parsed response data.
+ * @throws {Error} Throws an error if the fetch request or JSON parsing fails and displays an alert with the error message.
+ */
+
 export async function doFetch(url) {
     try {
         const response = await authFetch(url);
@@ -7,5 +22,7 @@ export async function doFetch(url) {
         return json;
     } catch (error) {
         throw new Error(error)
+        alert(error)
     }
 }
+
