7444494a2f616cc3b3c2aa0403946042df17543e
Jon Are <jon.are.br@gmail.com>
2024-05-14 23:14:50 +0200

    chore: Add reactToPost API endpoint and navigateToProfile listener

--- js/api/posts/index.mjs
+++ js/api/posts/index.mjs
@@ -1,6 +1,6 @@
 export * from "./create.mjs";
+export * from "./reactToPost.mjs";
 export * from "./update.mjs";
 export * from "./delete.mjs";
 export * from "./getPost.mjs";
 export * from "./getPosts.mjs";
-export * from "./reactToPost.mjs";
