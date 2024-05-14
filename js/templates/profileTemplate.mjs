613d0b1fe30b5b01a181848d77b9213ee9b39d2d
Jon Are <jon.are.br@gmail.com>
2024-05-14 23:14:14 +0200

    chore: Refactor Bootstrap script tags to use CDN links for faster loading

--- js/templates/profileTemplate.mjs
+++ js/templates/profileTemplate.mjs
@@ -2,53 +2,57 @@ import { navigateToProfile } from "../listeners/navigateToProfile.mjs";
 
 export function profileTemplate(profileData) {
     const profileContainer = document.createElement("div");
-    profileContainer.classList.add("card", "mx-auto", "border", "border-3", "rounded", "position-relative", "col-10", "col-md-10", "col-lg-8", "col-xl-6");
+    profileContainer.classList.add("card", "bg-primary", "px-0", "mx-auto", "border", "border-3", "rounded", "position-relative", "col-12");
 
     // Banner Image
     const bannerImg = document.createElement("img");
-    bannerImg.classList.add("card-img");
-    bannerImg.style.width = "100%";
+    bannerImg.classList.add("img-fluid", "card-img-top", "w-100", "px-0");
     bannerImg.style.height = "200px";
-    bannerImg.style.objectFit = "cover";
     bannerImg.src = profileData.banner || "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?q=80&w=1111&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     bannerImg.alt = "Banner";
-
-    // Profile Image (Clickable to navigate to profile)
-    const img = document.createElement("img");
-    img.classList.add("rounded", "position-absolute", "top-0", "start-50", "translate-middle", "rounded-1", "object-fit-cover");
-    img.style.width = "8rem";
-    img.style.height = "8rem";
-    img.src = profileData.avatar || "https://upload.wikimedia.org/wikipedia/commons/9/9d/Unknown_Member.jpg?20170805162126";
-    img.alt = `Profile of ${profileData.name}`;
-    img.style.cursor = "pointer";  // Indicate that it's clickable
-    img.addEventListener('click', () => navigateToProfile(profileData.name));  // Add click event
-
+    
     // Other elements remain the same
     const cardBody = document.createElement('div');
     cardBody.classList.add('card-body', 'py-0');
 
     const flexContainer = document.createElement('div');
-    flexContainer.classList.add('d-flex', 'flex-row', 'justify-content-between');
+    flexContainer.classList.add('d-flex', 'flex-row', 'justify-content-between', 'align-items-center');
 
     const followersDiv = document.createElement('div');
+    followersDiv.classList.add('text-center', 'border', 'border-2', 'rounded-3', 'p-2', 'bg-secondary', 'text-white');
     const followersCount = document.createElement('p');
-    followersCount.classList.add('fw-bold', 'fs-2', 'fw-bold');
-    followersCount.textContent = "203K";
+    followersCount.classList.add('fw-bold', 'fs-2', 'fw-bold', "my-0");
+    followersCount.textContent = profileData._count.followers;
+
     const followersText = document.createElement('p');
-    followersText.classList.add('text-capitalize', 'justify-content-start');
+    followersText.classList.add('text-capitalize');
     followersText.textContent = "followers";
+
     followersDiv.append(followersCount, followersText);
 
+    // Profile Image (Clickable to navigate to profile)
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add("p-1", "col-8", "col-md-10");
+    const img = document.createElement("img");
+    img.classList.add("rounded-3", "img-fluid", "p-1");
+    img.src = profileData.avatar || "https://upload.wikimedia.org/wikipedia/commons/9/9d/Unknown_Member.jpg?20170805162126";
+    img.alt = `Profile of ${profileData.name}`;
+    img.style.cursor = "pointer"; 
+    img.addEventListener('click', () => navigateToProfile(profileData.name)); 
+
+    imgContainer.append(img);
+
     const followingDiv = document.createElement('div');
+    followingDiv.classList.add("text-center", "border", "border-2", "rounded-3", "p-2", "bg-secondary", "text-white");
     const followingCount = document.createElement('p');
-    followingCount.classList.add('fw-bold', 'fs-2', 'fw-bold');
-    followingCount.textContent = "1.3K";
+    followingCount.classList.add('fw-bold', 'fs-2', "my-0");
+    followingCount.textContent = profileData._count.following;
     const followingText = document.createElement('p');
     followingText.classList.add('text-capitalize');
     followingText.textContent = "following";
     followingDiv.append(followingCount, followingText);
 
-    flexContainer.append(followersDiv, followingDiv);
+    flexContainer.append(followersDiv, imgContainer, followingDiv);
 
     const title = document.createElement('h4');
     title.classList.add('card-title', 'text-center', 'fw-bold');
@@ -64,14 +68,16 @@ export function profileTemplate(profileData) {
     const followButton = document.createElement('button');
     followButton.type = "button";
     followButton.classList.add('btn', 'btn-dark', 'rounded-4', 'py-2', 'px-4');
-    followButton.textContent = "follow";
+    followButton.id = "followButton";
+    followButton.textContent = "Follow";
 
-    const messageButton = document.createElement('button');
-    messageButton.type = "button";
-    messageButton.classList.add('btn', 'btn-outline-dark', 'rounded-4', 'py-2', 'px-4');
-    messageButton.textContent = "message";
+    const unfollowButton = document.createElement('button');
+    unfollowButton.type = "button";
+    unfollowButton.classList.add('btn', 'btn-outline-dark', 'rounded-4', 'py-2', 'px-4');
+    unfollowButton.id = "unfollowButton";
+    unfollowButton.textContent = "Unfollow";
 
-    buttonContainer.append(followButton, messageButton);
+    buttonContainer.append(followButton, unfollowButton);
 
     const navContainer = document.createElement('div');
     navContainer.classList.add('d-flex', 'justify-content-around', 'p-3');
@@ -94,117 +100,7 @@ export function profileTemplate(profileData) {
     navContainer.append(allButton, photosButton, videosButton);
 
     cardBody.append(flexContainer, title, description, buttonContainer, navContainer);
-    profileContainer.append(bannerImg, img, cardBody);
+    profileContainer.append(bannerImg, cardBody);
 
     return profileContainer;
 }
-
-
-
-
-
-// export function profileTemplate(profileData) {
-
-//     const profileContainer = document.createElement("div");
-//     profileContainer.classList.add("card", "mx-auto", "border", "border-3", "rounded", "position-relative", "col-10", "col-md-10", "col-lg-8", "col-xl-6");
- 
-//     // const link = document.createElement("a");
-//     // link.href = `?name=${profileData.name}`; 
-//     // link.className = "d-block";
-
-//         // Banner Image
-//     const bannerImg = document.createElement("img");
-//     bannerImg.classList.add("card-img");
-//     bannerImg.style.width = "100%"; // Cover the entire card width
-//     bannerImg.style.height = "200px"; // Fixed height for the banner
-//     bannerImg.style.objectFit = "cover"; // Ensure the image covers the area without distortion
-//     bannerImg.src = profileData.banner; // Placeholder banner image
-//     bannerImg.alt = "Banner";
-
-//     const img = document.createElement("img");
-//     img.classList.add("rounded", "position-absolute", "top-0", "start-50", "translate-middle", "rounded-1", "object-fit-cover");
-//     img.style.width = "8rem";
-//     img.style.height = "8rem";
-//     if (profileData.avatar) {
-//         img.src = profileData.avatar;
-//     } else {
-//         img.src = "https://upload.wikimedia.org/wikipedia/commons/9/9d/Unknown_Member.jpg?20170805162126";
-//     }
-//     img.alt = `Profile of ${profileData.name}`;
-
-//     const cardBody = document.createElement('div');
-//     cardBody.classList.add('card-body', 'py-0');
-
-//     const flexContainer = document.createElement('div');
-//     flexContainer.classList.add('d-flex', 'flex-row', 'justify-content-between');
-
-//     const followersDiv = document.createElement('div');
-//     const followersCount = document.createElement('p');
-//     followersCount.classList.add('fw-bold', 'fs-2', 'fw-bold');
-//     followersCount.textContent = "203K";
-//     const followersText = document.createElement('p');
-//     followersText.classList.add('text-capitalize', 'justify-content-start');
-//     followersText.textContent = "followers";
-
-//     followersDiv.append(followersCount, followersText);
-
-//     const followingDiv = document.createElement('div');
-//     const followingCount = document.createElement('p');
-//     followingCount.classList.add('fw-bold', 'fs-2', 'fw-bold');
-//     followingCount.textContent = "1.3K";
-//     const followingText = document.createElement('p');
-//     followingText.classList.add('text-capitalize');
-//     followingText.textContent = "following";
-
-//     followingDiv.append(followingCount, followingText);
-
-//     flexContainer.append(followersDiv, followingDiv);
-
-//     const title = document.createElement('h4');
-//     title.classList.add('card-title', 'text-center', 'fw-bold');
-//     title.textContent = `@${profileData.name}`;
-
-//     const description = document.createElement('p');
-//     description.classList.add('text-center', 'fs-5');
-//     description.textContent = `My name is ${profileData.name} and I like to teleport to space!`;
-
-//     const buttonContainer = document.createElement('div');
-//     buttonContainer.classList.add('d-flex', 'flex-row', 'justify-content-around');
-
-//     const followButton = document.createElement('button');
-//     followButton.type = "button";
-//     followButton.classList.add('btn', 'btn-dark', 'rounded-4', 'py-2', 'px-4');
-//     followButton.textContent = "follow";
-
-//     const messageButton = document.createElement('button');
-//     messageButton.type = "button";
-//     messageButton.classList.add('btn', 'btn-outline-dark', 'rounded-4', 'py-2', 'px-4');
-//     messageButton.textContent = "message";
-
-//     buttonContainer.append(followButton, messageButton);
-
-//     const navContainer = document.createElement('div');
-//     navContainer.classList.add('d-flex', 'justify-content-around', 'p-3');
-
-//     const allButton = document.createElement('button');
-//     allButton.type = "button";
-//     allButton.classList.add('btn', 'btn-outline-dark', 'text-capitalize', 'border-0');
-//     allButton.textContent = "all";
-
-//     const photosButton = document.createElement('button');
-//     photosButton.type = "button";
-//     photosButton.classList.add('btn', 'border-bottom', 'text-capitalize', 'link-underline');
-//     photosButton.textContent = "photos";
-
-//     const videosButton = document.createElement('button');
-//     videosButton.type = "button";
-//     videosButton.classList.add('btn', 'btn-outline-dark', 'text-capitalize', 'border-0');
-//     videosButton.textContent = "videos";
-
-//     navContainer.append(allButton, photosButton, videosButton);
-
-//     cardBody.append(flexContainer, title, description, buttonContainer, navContainer);
-//     profileContainer.append(bannerImg, img, cardBody);
-
-//     return profileContainer;
-// }
