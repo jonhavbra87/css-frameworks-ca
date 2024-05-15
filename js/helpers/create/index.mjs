3a1f0952cac289ba38e70b4762c8c24bcd42f7ac
Jon Are <jon.are.br@gmail.com>
2024-05-15 17:23:27 +0200

    feat: Add profile creation components and update profile template
    Refactor the profileTemplate to use helper functions for improved readability and maintainability. Add new components for profile creation, including createProfileBanner, createProfileBio, createProfileButtonContainer, createProfileCardBody, createProfileCardContainer, createProfileDescriptions, createProfileFlexContainer, createProfileFollowersDiv, createProfileFollowingDiv, createProfileImage, createProfileNav, and createProfileTitle. These changes enhance the profile page functionality and layout.

--- js/helpers/create/index.mjs
+++ js/helpers/create/index.mjs
@@ -6,4 +6,16 @@ export * from "./createIconElement.mjs";
 export * from "./createMediaContainer.mjs";
 export * from "./createUserInfoContainer.mjs";
 export * from "./createImgWrapperContainer.mjs";
-export * from "./createCommentsContainer.mjs"
+export * from "./createCommentsContainer.mjs";
+export * from "./createProfileBanner.mjs";
+export * from "./createProfileBio.mjs";
+export * from "./createProfileButtonContainer.mjs";
+export * from "./createProfileCardBody.mjs";
+export * from "./createProfileCardContainer.mjs";
+export * from "./createProfileDescriptions.mjs";
+export * from "./createProfileFlexContainer.mjs";
+export * from "./createProfileFollowersDiv.mjs";
+export * from "./createProfileFollowingDiv.mjs";
+export * from "./createProfileImage.mjs";
+export * from "./createProfileNav.mjs";
+export * from "./createProfileTitle.mjs";
