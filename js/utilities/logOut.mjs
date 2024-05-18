d2dbeca9349699d385868685319b556a57a68fbc
Jon Are <jon.are.br@gmail.com>
2024-05-19 00:58:09 +0200

    Refactor logOut functionality to redirect user to the feed page

--- js/utilities/logOut.mjs
+++ js/utilities/logOut.mjs
@@ -2,7 +2,7 @@
  * Sets up a log out functionality.
  * 
  * This function adds a click event listener to the log out button. When the button is clicked,
- * it clears the local storage to remove all user data and redirects the user to the home page.
+ * it clears the local storage to remove all user data and redirects the user to the feed page.
  *
  * @function logOut - Sets up a log out functionality.
  * @returns {void} - No return value
