e963415a5697f3e22e72291d32f738823b0d18d7
Jon Are <jon.are.br@gmail.com>
2024-05-19 00:01:29 +0200

    Refactor renderSpecificPost to improve code modularity and readability

---
+++ js/templates/renderSpecificPost.mjs
@@ -0,0 +1,22 @@
+import { buttonContainer } from "../helpers/create/buttonContainer.mjs";
+import { postTemplate } from "./postTemplate.mjs";
+
+//sjekke om man er logget inn if-statement
+
+export function renderSpecificPost(postData, parent) {
+
+    const card = postTemplate(postData);
+    const name = localStorage.getItem("profile")
+    const profile = JSON.parse(name);
+ 
+    const profileName = profile.name;
+    const authorName = postData.author.name;
+
+    parent.append(card);
+
+    if (authorName === profileName) {
+        const buttons = buttonContainer(postData);
+        
+        card.append(buttons);
+    }
+}
